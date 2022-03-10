f449c554c4d291f8106270658d8c5e9c9bca0178
James Lavery <jamestlavery@gmail.com>
2022-03-10 22:22:07 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Blurdle</h1>
+    <p>You have 5 guesses to guess the image. The image gets clearer after every guess.</p>
+    <hr>
+    <div style="margin-top:1em;">
+      <p style="font-size:20px;" v-if="!this.gameOver && !this.correct">Guesses left: {{ this.guessCount }}</p>
+      <p class="red" v-if="this.guessEmpty">PLEASE ENTER A GUESS</p>
+      <p class="red" v-if="this.incorrect">INCORRECT</p>
+
+      <div v-if="this.gameOver" style="margin-bottom:1em;">
+        <p class="red">GAME OVER. THE IMAGE WAS OF A {{ this.imageToShow.answer.toUpperCase() }}</p>
+        <button @click="playAgain()">Play again</button>
+      </div>
+
+      <div v-if="this.correct" style="margin-bottom:1em;">
+        <p class="green" v-if="this.correct">CORRECT, THE IMAGE WAS OF A {{ this.imageToShow.answer.toUpperCase() }}!</p>
+        <button @click="playAgain()">Play again</button>
+      </div>
+      
+      <img :src="require(`${this.imageToShow.src}`)"  width="300" height="300" :class="`blur-${guessCount}`">
+      <p><input type="text" name="guess" placeholder="Enter guess..." v-model="guess" :disabled="this.disabled"/><br>
+      <button @click="validateAnswer()" :disabled="this.disabled">Submit</button></p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  return Math.floor(Math.random() * (Math.floor(max) - min) + min);
+}
+
+import { images } from './images.js';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      class: 'blur-5',
+      guessCount: 5,
+      guess: '',
+      guessEmpty: false,
+      incorrect: false,
+      correct: false,
+      disabled: false,
+      gameOver: false,
+      imageToShow: images[getRandomInt(0, images.length)],
+    }
+  },
+  methods: {
+    validateAnswer() {
+      this.guessEmpty = false;
+      this.incorrect = false;
+
+      if (this.guess === '') {
+        this.guessEmpty = true;
+        return;
+      }
+
+      if (this.guess.toLowerCase() === this.imageToShow.answer.toLowerCase()) {
+        this.correct = true;
+        this.disabled = true;
+        this.guessCount = 0;
+        return;
+      } else {
+        this.incorrect = true;
+        this.guessCount--;
+        this.guess = '';
+      }
+
+      if (this.guessCount == 0) {
+        this.gameOver = true;
+        this.disabled = true;
+        return;
+      }
+    },
+    playAgain() {
+      this.guess = '';
+      this.disabled = false;
+      this.correct = false;
+      this.incorrect = false;
+      this.guessCount = 5;
+      this.gameOver = false;
+
+      this.imageToShow = images[getRandomInt(0, images.length)];
+    }
+  },
 }
 </script>
 
@@ -23,6 +96,49 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 1em;
+}
+
+.blur-5{
+  filter: blur(5rem);
+}
+.blur-4{
+  filter: blur(4rem);
+}
+.blur-3{
+  filter: blur(3rem);
+}
+.blur-2{
+  filter: blur(2rem);
+}
+.blur-1{
+  filter: blur(1rem);
+}
+.blur-0{
+  filter: blur(0rem);
+}
+.red {
+  font-size:20px;
+  color:red;
+}
+.green {
+  font-size:20px;
+  color:green;
+}
+button {
+  border:1px solid grey;
+  background:white;
+  color:black;
+  margin-top:0.2em;
+  padding:1em;
+}
+button:hover {
+  background:green;
+  color:white;
+  cursor: pointer;
+}
+input {
+  padding:0.5em;
+  font-size:20px;
 }
 </style>
---
+++ src/images.js
@@ -0,0 +1,26 @@
+const images = [
+    {src: './img/image1.jpg', answer: 'Lion'},
+    {src: './img/image2.jpg', answer: 'Bicycle'},
+    {src: './img/image3.jpg', answer: 'House'},
+    {src: './img/image4.jpg', answer: 'Gorilla'},
+    {src: './img/image5.jpg', answer: 'Whale'},
+    {src: './img/image6.jpg', answer: 'Wheelie Bin'},
+    {src: './img/image7.jpg', answer: 'Tree'},
+    {src: './img/image9.jpg', answer: 'Flower'},
+    {src: './img/image10.jpg', answer: 'Beach'},
+    {src: './img/image8.jpg', answer: 'Cake'},
+    {src: './img/image11.jpg', answer: 'Football'},
+    {src: './img/image12.jpg', answer: 'Penguin'},
+    // {src: './img/', answer: 'White House'},
+    // {src: './img/', answer: 'Sweets'},
+    // {src: './img/', answer: 'Car'},
+    // {src: './img/', answer: 'Train'},
+    // {src: './img/', answer: 'Castle'},
+    // {src: './img/', answer: 'Traffic light'},
+    // {src: './img/', answer: 'Shoes'},
+    // {src: './img/', answer: 'London Eye'},
+]
+
+export {
+    images
+}
